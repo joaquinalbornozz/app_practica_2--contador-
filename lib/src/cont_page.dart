dfb9ae0c7ae73c6eba35d84413cf135186228e8a
joaquinalbornozz <joaquinealbornoz021@gmail.com>
2025-08-27 00:01:53 -0300

    practica_2

--- lib/src/cont_page.dart
+++ lib/src/cont_page.dart
@@ -3,15 +3,6 @@ import 'package:flutter/material.dart';
 class ContPage extends StatefulWidget {
   const ContPage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -23,97 +14,76 @@ class _ContPageState extends State<ContPage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
-  void _restar(){
-    setState((){
+  void _restar() {
+    setState(() {
       _counter--;
     });
   }
 
-  void _multimplicar(){
-    setState((){
-      _counter=_counter*2;
+  void _multimplicar() {
+    setState(() {
+      _counter = _counter * 2;
     });
   }
-  void _cerear(){
-    setState((){
-      _counter=0;
+
+  void _cerear() {
+    setState(() {
+      _counter = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const Text('Número de clicks :'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            ElevatedButton(onPressed: _multimplicar, child: const Text('Multiplicar por 2'))
+            ElevatedButton(
+                onPressed: _multimplicar,
+                child: const Text('Multiplicar por 2'))
           ],
         ),
       ),
-      floatingActionButton: _botones(), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: _botones(),
     );
   }
 
-  Widget _botones(){
-    return  Column(
-      //mainAxisAlignment: MainAxisAlignment.end,
+  Widget _botones() {
+    return Column(
       children: <Widget>[
         const Expanded(child: SizedBox()),
         FloatingActionButton(
-        onPressed: _restar,tooltip: 'Decrement', backgroundColor: Colors.greenAccent, child: const Icon(Icons.remove)),
-        const SizedBox(width: 30.0),
-        FloatingActionButton(onPressed: _incrementCounter, tooltip: 'Increment', backgroundColor: Colors.greenAccent, child: const Icon(Icons.add)),
+            onPressed: _restar,
+            tooltip: 'Decrementar',
+            backgroundColor: Colors.greenAccent,
+            child: const Icon(Icons.remove)),
         const SizedBox(width: 30.0),
         FloatingActionButton(
-        onPressed: _cerear, tooltip: 'Zero', backgroundColor: Colors.greenAccent, child: const Icon(Icons.exposure_zero)),
- ],
+            onPressed: _incrementCounter,
+            tooltip: 'Incrementar',
+            backgroundColor: Colors.greenAccent,
+            child: const Icon(Icons.add)),
+        const SizedBox(width: 30.0),
+        FloatingActionButton(
+            onPressed: _cerear,
+            tooltip: 'Cero',
+            backgroundColor: Colors.greenAccent,
+            child: const Icon(Icons.exposure_zero)),
+      ],
     );
   }
 }
